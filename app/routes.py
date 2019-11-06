09247f1c19706c338d5eed17fb8eb2c962738a0a
Manoharan <144725@cognizant.com>
2019-11-06 16:19:20 -0500

    work with multiple files and extract email ids

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,39 @@
-from app import app
+from app import app, api
 from flask import render_template, flash, request, url_for
-from app.forms import BrowsingForm
+from flask_restful import Resource
 from config import Config
-from main import get_skills
+from main import get_skills, Candidate, process_candidate
 import os
+
+# class Index(Resource):
+#     def get(self):
+#         return render_template("upload.html")
+
+# class Upload(Resource):
+#     def get(self):
+#         skills = []
+#         target = os.path.join(Config.APP_ROOT, 'temp')
+#         print("target folder", target)
+
+#         if not os.path.isdir(target):
+#             os.mkdir(target)
+
+#         for file in request.files.getlist("file"):
+#             print("file name " , file)
+#             filename = file.filename
+#             destination = "\\".join([target,filename])
+#             print("destination folder ", destination)
+#             file.save(destination)
+#             skills.extend(get_skills(destination))
+#             print(skills)
+
+#         return render_template('completed.html', skills=skills)
+
+
+# api.add_resource(Index,'/')
+# api.add_resource(Upload,'/upload')
+
+
 
 @app.route('/')
 def index():
@@ -11,6 +41,7 @@ def index():
 
 @app.route('/upload',methods=['GET','POST'])
 def upload():
+    candidates = []
     skills = []
     target = os.path.join(Config.APP_ROOT, 'temp')
     print("target folder", target)
@@ -19,11 +50,24 @@ def upload():
         os.mkdir(target)
 
     for file in request.files.getlist("file"):
+        '''TODO : Write logic to separte the skills per file
+                  Right now. It combines all the skills into 1 list while showing
+        '''
+
         print("file name " , file)
         filename = file.filename
         destination = "\\".join([target,filename])
         print("destination folder ", destination)
         file.save(destination)
-        skills.extend(get_skills(destination))
-        print(skills)
-    return render_template('completed.html', skills=skills)
+        
+        #skills.extend(get_skills(destination))
+        #print(skills)
+
+        #extract email
+        candidate = process_candidate(path=destination)
+        print('Canddiate Email ID : {}'.format(candidate.email_id))
+        print('Candidate skills : {}'.format(candidate.skills))
+        candidates.append(candidate)
+        #TODO : Write code to delete the file from the temp folder
+
+    return render_template('completed.html', skills=skills, candidates=candidates)
--- main.py
+++ main.py
@@ -2,7 +2,39 @@ from utility import get_stop_words, get_prgm_lang_list, extract_pdfToText_from_f
 import docx2txt
 import spacy
 import os
+import re
 
+class Candidate:
+    def __init__(self):
+        self.name = ''
+        self.email_id = ''
+        self.contact_number = ''
+        self.resume_text = ''
+        self.skills = []
+
+def process_candidate(path):
+    candidate = Candidate()
+
+    master_skill_list = get_prgm_lang_list()
+    stop_words = get_stop_words()
+    resume_text = get_resume_text(path)
+
+    nlp = spacy.load('en_core_web_sm')
+    nlp_ed = nlp(resume_text)
+    n_phrases = [token.text.strip().lower() for token in nlp_ed.noun_chunks
+                 if token.text not in stop_words]
+    skills = [skill for skill in n_phrases if skill in master_skill_list]
+    candidate.skills = list(set(skills))
+
+
+    resume_text = resume_text.replace('\n','')
+    #print(resume_text)
+    match = re.search(r'[\w\.-]+@[\w\.-]+',resume_text)
+    if not match is None:
+        candidate.email_id = match.group(0)
+        print(candidate.email_id)
+    
+    return candidate
 
 def get_skills(path):
     master_skill_list = get_prgm_lang_list()
@@ -31,5 +63,5 @@ def get_resume_text(path):
         elif path[-3:] == 'pdf':    
             print('Its a pdf file')
             resume_text = extract_pdfToText_from_file(path)
-            # print(resume_text)
+            #print(resume_text)
     return resume_text
